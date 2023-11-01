5b67d375811f3afd553c12281a207cc08cff044b
Matthew Periut <slaincow@gmail.com>
2023-10-31 21:28:36 -0400

    Update to SPC 0.4.0 Bump version

--- src/main/java/com/slainlight/midastouch/command/GoldenCommand.java
+++ src/main/java/com/slainlight/midastouch/command/GoldenCommand.java
@@ -1,21 +1,25 @@
 package com.slainlight.midastouch.command;
 
 import com.matthewperiut.spc.api.Command;
+import com.matthewperiut.spc.util.SharedCommandSource;
 import com.slainlight.midastouch.entity.GoldenEntity;
 import net.minecraft.entity.EntityBase;
 import net.minecraft.entity.Living;
 import net.minecraft.entity.player.PlayerBase;
+import net.minecraft.util.maths.Box;
 
-import static com.matthewperiut.spc.util.SPChatUtil.sendMessage;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class GoldenCommand implements Command
 {
     @Override
-    public void command(PlayerBase player, String[] parameters)
+    public void command(SharedCommandSource commandSource, String[] parameters)
     {
         if (parameters.length < 2)
         {
-            manual();
+            manual(commandSource);
             return;
         }
 
@@ -27,12 +31,12 @@ public class GoldenCommand implements Command
         }
         catch (Exception e)
         {
-            sendMessage("Invalid entity id");
+            commandSource.sendFeedback("Invalid entity id");
             return;
         }
 
         EntityBase target = null;
-        for (Object o : player.level.entities)
+        for (Object o : commandSource.getPlayer().level.entities)
         {
             EntityBase e = (EntityBase) o;
 
@@ -44,14 +48,14 @@ public class GoldenCommand implements Command
 
         if (target == null)
         {
-            sendMessage("Target not found");
+            commandSource.sendFeedback("Target not found");
             return;
         }
 
         if (target instanceof Living living)
         {
-            ((GoldenEntity) (Object) living).midastouch$setGolden(!((GoldenEntity) (Object) living).midastouch$getGolden());
-            sendMessage("Golden: " + ((GoldenEntity) (Object) living).midastouch$getGolden());
+            ((GoldenEntity) living).midastouch$setGolden(!((GoldenEntity) living).midastouch$getGolden());
+            commandSource.sendFeedback("Golden: " + ((GoldenEntity) living).midastouch$getGolden());
         }
     }
 
@@ -62,8 +66,48 @@ public class GoldenCommand implements Command
     }
 
     @Override
-    public void manual()
+    public void manual(SharedCommandSource commandSource)
     {
-        sendMessage("Usage: /golden {entity id}");
+        commandSource.sendFeedback("Usage: /golden {entity id}");
+    }
+
+    @Override
+    public String[] suggestion(SharedCommandSource source, int parameterNum, String currentInput, String totalInput) {
+        if (parameterNum == 1) {
+            PlayerBase p = source.getPlayer();
+            List<EntityBase> entities = p.level.getEntities(EntityBase.class, Box.create(p.x-20, p.y-20, p.z-20, p.x+20, p.y+20, p.z+20));
+
+            // Use TreeMap to keep entries in order based on the distance
+            TreeMap<Double, EntityBase> distanceMap = new TreeMap<>();
+
+            for (EntityBase entity : entities) {
+                double distance = p.distanceTo(entity);
+                // Handle potential duplicates (unlikely but possible)
+                while (distanceMap.containsKey(distance)) {
+                    distance += 0.0001;  // Small offset to handle entities at almost same distance
+                }
+                distanceMap.put(distance, entity);
+            }
+
+            // Extract entity IDs from sorted entities and convert them to String
+            // If entity is a PlayerBase, use getName() instead
+            List<String> sortedEntityIDs = distanceMap.values().stream()
+                    .filter(entityBase -> !(entityBase instanceof PlayerBase))
+                    .filter(entityBase -> entityBase instanceof Living)
+                    .map(entity -> Integer.toString(entity.entityId))
+                    .collect(Collectors.toList());
+
+            // Filter and modify the suggestions based on currentInput
+            for (int i = sortedEntityIDs.size() - 1; i >= 0; i--) {  // Change loop condition to i >= 0
+                if (!sortedEntityIDs.get(i).startsWith(currentInput)) {
+                    sortedEntityIDs.remove(i);
+                } else {
+                    sortedEntityIDs.set(i, sortedEntityIDs.get(i).substring(currentInput.length()));
+                }
+            }
+
+            return sortedEntityIDs.toArray(new String[0]);
+        }
+        return new String[0];
     }
 }
